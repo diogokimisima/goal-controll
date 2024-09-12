a32904bcd5e9fa38810b316704dd08168e2819ea
diogokimisima <dkimisima@gmail.com>
2024-09-12 11:07:32 -0300

    creating the get week pending goals

---
+++ backend/src/functions/get-week-pending-goals.ts
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import { db } from "../db";
+import { goalCompletions, goals } from "../db/schema";
+import { count, lte } from "drizzle-orm";
+
+dayjs.extend(weekOfYear);
+
+export function getWeekPendingGoals() {
+  const lastDayOfWeek = dayjs().endOf("week").toDate();
+
+  const goalsCreatedUpToWeek = db.$with("goals_created_up_week").as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  );
+
+  const goalCompletionCounts = db.$with("goal_completion_counts").as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id),
+      })
+      .from(goalCompletions)
+      .groupBy(goalCompletions.goalId)
+  );
+}
