8766ff76c451c0dc7f3a99049b8352a5be645a55
diogokimisima <dkimisima@gmail.com>
2024-09-16 22:19:01 -0300

    finishing modal component and creating summary component

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,83 +1,15 @@
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Plus, X } from 'lucide-react'
-import { Button } from './components/ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from './components/ui/dialog'
-import { Label } from './components/ui/label'
-import { Input } from './components/ui/input'
-import { RadioGroup, RadioGroupItem } from './components/ui/radio-group'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+// import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit" />
-        <img src={letsStart} alt="lets start illustration" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
+      {/* <EmptyGoals /> */}
+      <Summary />
 
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
-
-      <DialogContent>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center justify-between">
-              <DialogTitle>Cadastrar meta</DialogTitle>
-              <DialogClose>
-                <X className="size-5 text-zinc-600" />
-              </DialogClose>
-            </div>
-
-            <DialogDescription>
-              Adicione atividades que te fazem bem e que você quer continuar
-              praticando toda semana.
-            </DialogDescription>
-          </div>
-
-          <form action="" className="flex-1 flex flex-col justify-between">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Qual a atividade?</Label>
-                <Input
-                  id="title"
-                  autoFocus
-                  placeholder="Praticar exercícios, meditar, etc..."
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Quantas vezes na semana?</Label>
-                <RadioGroup>
-                  <RadioGroupItem value="1">1x na semana</RadioGroupItem>
-                </RadioGroup>
-              </div>
-            </div>
-
-            <div className="flex items-center gap-3">
-              <DialogClose asChild>
-                <Button type="button" className="flex-1" variant="secondary">
-                  Fechar
-                </Button>
-              </DialogClose>
-              <Button className="flex-1">Salvar</Button>
-            </div>
-          </form>
-        </div>
-      </DialogContent>
+      <CreateGoal />
     </Dialog>
   )
 }
---
+++ frontend/src/components/summary.tsx
@@ -0,0 +1,30 @@
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import { InOrbitIcon } from './in-orbit-icon'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+
+export function Summary() {
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de agosto</span>
+        </div>
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator />
+        </Progress>
+      </div>
+    </div>
+  )
+}
