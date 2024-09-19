8e6706fd9593d95d47c3e68bb8b62faa6b8125d0
diogokimisima <dkimisima@gmail.com>
2024-09-19 19:57:33 -0300

    finishing get-pending-goals

--- frontend/src/http/get-pending-goals.tsx
+++ frontend/src/http/get-pending-goals.tsx
@@ -9,5 +9,5 @@ export async function getPendingGoals(): Promise<PendingGoalsResponse> {
   const response = await fetch('http://localhost:3333/pending-goals')
   const data = await response.json()
 
-  return data.summary
+  return data.pendingGoals
 }
