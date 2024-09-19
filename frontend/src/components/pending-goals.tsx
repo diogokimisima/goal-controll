02e6a1a149e56a7ad36d9cfb978610abd5980ad5
diogokimisima <dkimisima@gmail.com>
2024-09-19 19:49:00 -0300

    finishing get-summary

---
+++ frontend/src/components/pending-goals.tsx
@@ -0,0 +1,23 @@
+import { Plus } from 'lucide-react'
+import { OutlineButton } from './ui/outline-button'
+
+export function PendingGoals() {
+  return (
+    <div className="flex flex-wrap gap-3">
+      <OutlineButton>
+        <Plus className="size-4 text-zinc-600" />
+        Meditar
+      </OutlineButton>
+
+      <OutlineButton>
+        <Plus className="size-4 text-zinc-600" />
+        Nadar
+      </OutlineButton>
+
+      <OutlineButton>
+        <Plus className="size-4 text-zinc-600" />
+        Praticar exercício
+      </OutlineButton>
+    </div>
+  )
+}
