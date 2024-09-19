a83d584b26fccfe749b21b9cb7a9d6422dcc6d1d
diogokimisima <dkimisima@gmail.com>
2024-09-19 13:11:44 -0300

    add get-summary using the tanStack query

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,38 +1,33 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
-import { useEffect, useState } from 'react'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completed: string
-    }[]
-  >
-}
+
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+  // const [summary, setSummary] = useState<SummaryResponse | null>(null)
 
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setSummary(data.summary)
-      })
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/summary')
+  //     .then(response => {
+  //       return response.json()
+  //     })
+  //     .then(data => {
+  //       setSummary(data.summary)
+  //     })
+  // }, [])
+
+  const {data} = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
 
   return (
     <Dialog>
-      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
